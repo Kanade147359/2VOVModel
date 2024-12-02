7c1dc518be55b780cc1d8440bf4f0c6f05709fc5
kanade147359 <kanade147359@keio.jp>
2024-12-02 13:18:41 +0900

    while kを追加してみた

--- src/initial_values.cpp
+++ src/initial_values.cpp
@@ -6,19 +6,21 @@ void generate_initial_positions(double positions[][2],int row_size, int num, dou
 
     int k=0;
     int col_size = row_size;
-    for (int i = 0; i < col_size; ++i)
-    {
-        for (int j = 0; j < row_size; ++j) {
+    while (k < num) {
+        for (int i = 0; i < col_size; ++i)
+        {
+            for (int j = 0; j < row_size; ++j) {
 
-            if (i % 2 == 0) {
-                positions[k][X] = std::sqrt(3)*r/2 * i + r/2;
-                positions[k][Y] = r * j + r/2 ;
-                ++k;
-            }
-            else {
-                positions[k][X] = std::sqrt(3)*r/2 * i + r/2;
-                positions[k][Y] = r * j + r / 2 + r / 2;
-                ++k;
+                if (i % 2 == 0) {
+                    positions[k][X] = std::sqrt(3)*r/2 * i + r/2;
+                    positions[k][Y] = r * j + r/2 ;
+                    ++k;
+                }
+                else {
+                    positions[k][X] = std::sqrt(3)*r/2 * i + r/2;
+                    positions[k][Y] = r * j + r / 2 + r / 2;
+                    ++k;
+                }
             }
         }
     }
